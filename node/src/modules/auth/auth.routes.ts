846cbd9f1f7d0ab666366fe57ae4f865459e3984
Rafael Eziquiel <rafael.eziquiel.dev@gmail.com>
2026-08-17 21:40:46 -0300

    feat: resend register token backend
    - Added Use Cases Resend Register Token

--- node/src/modules/auth/auth.routes.ts
+++ node/src/modules/auth/auth.routes.ts
@@ -6,6 +6,7 @@ const router = express.Router();
 router.post("/login", authController.login);
 router.post("/register", authController.registerStart);
 router.post("/register-complete", authController.registerComplete);
+router.post("/resend-register-token", authController.resendRegisterToken);
 router.post("/forgot-password", authController.forgotPassword);
 router.post("/reset-password", authController.resetPassword);
 
